529b1e1be7483fdb17f58f2ec01b9764dfdc86eb
Henry <hrmstockdale@gmail.com>
2019-03-28 11:35:49 +0000

    xml file added to public

---
+++ public/fetchRequest.js
@@ -0,0 +1,12 @@
+var xhr = new XMLHttpRequest();
+
+function fetch(callback) {
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+    //   console.log(xhr.responseText);
+      callback(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.open("GET", "/src/mealPrep.js", true);
+  xhr.send();
+}
