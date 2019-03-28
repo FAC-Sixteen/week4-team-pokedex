e32d9dba848a1a10a12599fd08a22ce33aec3410
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-03-28 12:40:11 +0000

    added some test changes to handler

--- src/handler.js
+++ src/handler.js
@@ -1,37 +1,46 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const jungle = require("jungle.js");
+const sliceVegetables = require("sliceVegetables");
 
 function handler(request, response) {
-    const endpoint = request.url;
-    const types = {
-        html: 'text/html',
-        css: 'text/css',
-        js: 'applications/javascript',
-        ico: "image/x-icon"
-    }
-    const extension = endpoint.split('.')[1]; 
-        if (endpoint === '/') {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            fs.readFile(__dirname + '/../index.html', function(error, file){
-                if (error) {
-                    console.log(error);
-                    return;
-                }
-                response.end(file);
-            })
-        } else {
-            const filePath = path.join(__dirname, "..", "public", endpoint);
-            fs.readFile(filePath, function(error,file){
-                if(error){
-                    console.log(error);
-                    return;
-                }
-            response.writeHead(200, {"Content-Type":types[extension]})
-            response.end(file);
-            });
-        }
+  const endpoint = request.url;
+  const types = {
+    html: "text/html",
+    css: "text/css",
+    js: "applications/javascript",
+    ico: "image/x-icon",
+    json: "application/json"
+  };
+  const extension = endpoint.split(".")[1];
+  if (endpoint === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile(__dirname + "/../index.html", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  } else if (endpoint === "/plants") {
+    fs.readFile(__dirname + "/JSON/plants.json", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(JSON.parse(file));
+    });
+  } else {
+    const filePath = path.join(__dirname, "..", "public", endpoint);
+    fs.readFile(filePath, function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.writeHead(200, { "Content-Type": types[extension] });
+      response.end(file);
+    });
+  }
 }
-
-
 
 module.exports = handler;
