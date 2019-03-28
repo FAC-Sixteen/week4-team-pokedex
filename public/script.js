992110827d2dd246b756235a253d708c620395a7
banna <bannatekle@gmail.com>
2019-03-28 12:27:52 +0000

    script

---
+++ public/script.js
@@ -0,0 +1,19 @@
+let input = document.getElementById('inputBox')
+
+input.addEventListener('change', function(e){
+    const inputValue = e.target.value;
+ 
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if (xhr.status === 200){
+                console.log(xhr);
+
+            }
+        }
+    }
+
+    
+    xhr.open('GET', '/jungle', true);
+    xhr.send(inputValue);
+})
