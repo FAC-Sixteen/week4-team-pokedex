f0b61b7f6d7011c8aa9bdfc0088e86e2cd1ae40e
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-03-28 18:13:43 +0000

    made searchfilter work from the start of words

--- src/searchFilter.js
+++ src/searchFilter.js
@@ -1,6 +1,15 @@
 const searchFilter = (input, array) => {
-  console.log(array);
-  const a = array.filter(plant => plant.includes(input.toLowerCase()));
+  //console.log(array);
+  const a = array.filter(plant => {
+    const p = plant.split(" ");
+    for (let i = 0; i < p.length; i++) {
+      let b = p[i].includes(input.toLowerCase(), 0);
+      if (b == true) {
+        return true;
+      }
+    }
+  });
+  console.log(a);
   return a;
 };
 
